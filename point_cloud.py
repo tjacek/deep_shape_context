25d4b3775923a4138d5d8d0f54985443231c4fb9
tjacek <tjacek@student.agh.edu.pl>
2015-11-24 19:56:24 +0100

    find max and min points

--- point_cloud.py
+++ point_cloud.py
@@ -1,6 +1,20 @@
+import numpy as np
+
 class PointCloud(object):
     def __init__(self, points):
  	    self.points = points
+
+    def find_max(self):
+    	return self.find_extremum(max)
+
+    def find_min(self):
+    	return self.find_extremum(min)
+
+    def find_extremum(self,fun):
+        return [fun(self.get_dim(i)) for i in range(3)]
+
+    def get_dim(self,i):
+        return [point[i] for point in self.points]
 
 def create_point_cloud(array):
 	width=array.shape[0]
@@ -10,9 +24,12 @@ def create_point_cloud(array):
 		for y_j in range(height):
 			z=array[x_i][y_j]
 			if(z!=0):
-				points.append((x_i,y_j,z))
-	print(len(points))
-	return PointCloud(points)
+				new_point=np.array((x_i,y_j,z))
+				points.append(new_point)
+	point_cloud=PointCloud(points)
+	print(point_cloud.find_min())
+	print(point_cloud.find_max())
+	return point_cloud
 
 
 
