c4f5805451cb20c98aaf2fdc445ef9e3ea724f75
tjacek <tjacek@student.agh.edu.pl>
2015-11-26 15:52:30 +0100

    create action image

--- time_series.py
+++ time_series.py
@@ -1,18 +1,26 @@
 import utils
 import point_cloud as pc
+import pandas as pd
 import scipy.misc as image
+import numpy as np
 from sklearn.decomposition import PCA
 
 class FinalAction(object):
     def __init__(self,frames):
  	    self.frames=frames
 
+    def to_pca(self):
+        eigen=[frame.to_pca() for frame in self.frames]
+        return np.array(eigen)
+
 class Frame(object):
     def __init__(self, p_clouds):
         self.p_clouds=p_clouds
 
     def to_pca(self):
-        return [get_pca(p_cloud) for p_cloud in self.p_clouds]
+        raw_pca=[get_pca(p_cloud) for p_cloud in self.p_clouds]
+        eigen=np.array(raw_pca).flatten()
+        return eigen
 
 def read_action(path):
     names=utils.get_files(path+"xy/")
@@ -35,9 +43,31 @@ def get_pca(p_cloud):
     x=p_cloud.to_array()
     pca = PCA()
     pca.fit(x)
-    return pca.components_ 
+    return pca.explained_variance_ratio_ #pca.components_ 
 
+def to_time_serie(array):
+    length=array.shape[0]
+    dim=array.shape[1]
+    columns=['c'+str(i) for i in range(dim)]
+    index=range(length)
+    return pd.DataFrame(array,index=index,columns=columns)
+
+def to_img(time_series):
+    multipler=10
+    cols=time_series.columns
+    length=len(time_series)
+    dim=len(cols)*multipler
+    action_img=np.zeros((length,dim))
+    for i in range(length):
+        for j in range(dim):
+            chnl=cols[j/multipler]
+            action_img[i][j]=time_series[chnl][i]
+    return action_img
+ 
 if __name__ == "__main__":
     action_path="../show2/"
     action=read_action(action_path)
-    print(action.frames[0].to_pca())
+    pca=action.to_pca()
+    td=to_time_serie(pca)
+    img=to_img(td)
+    utils.save_img("../action",img)
