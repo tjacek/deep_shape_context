ab146734f5f41292582f506ef78ed25196977aa0
tjacek <tjacek@student.agh.edu.pl>
2015-11-27 22:30:40 +0100

    convert to point cloud

--- dsc.py
+++ dsc.py
@@ -1,5 +1,7 @@
 import utils
 import preproc
+import point_cloud
+import action
 
 def transform_files(in_path,out_path,transform):
     utils.make_dir(out_path)
@@ -7,15 +9,24 @@ def transform_files(in_path,out_path,transform):
     for name in names:
         full_in_path=in_path+name
         full_out_path=out_path+name
-        binar_to_raw(full_in_path,full_out_path)
+        transform(full_in_path,full_out_path)
 
-def binar_to_raw(in_path,out_path):
+def binary_to_raw(in_path,out_path):
     raw_action=preproc.read_binary(in_path)
     out_path=out_path.replace(".bin",".raw")
     print(out_path)
     utils.save_object(raw_action,out_path)
 
+def raw_to_pcloud(in_path,out_path):
+    pcloud=action.make_action(in_path)
+    pcloud.standarize()
+    out_path=out_path.replace(".raw",".cloud")
+    print(out_path)
+    utils.save_object(pcloud,out_path)
+
 if __name__ == "__main__":
-    in_path="../raw/"
-    out_path="../raw_actions/"
-    transform_files(in_path,out_path,binar_to_raw)
+    binary_path="../raw/"
+    raw_path="../raw_actions/"
+    cloud_path="../cloud_actions/"
+    #transform_files(binary_path,raw_path,binary_to_raw)
+    transform_files(raw_path,cloud_path,raw_to_pcloud)
